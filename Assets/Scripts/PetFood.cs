a6011ecd28af57784a087b502516480602210575
Aiko Mei McCarthy <mccarthyaiko@gmail.com>
2025-11-10 19:20:08 -0500

    Started Adding Food
    Scripts and prefabs

---
+++ Assets/Scripts/PetFood.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetFood : MonoBehaviour
+{
+    public int hungerGain = 20;
+    public int happinessGain = 5;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
